cdb97c6824fb822945eefb54ec8463979290dc85
Menn <m@seedwebs.com>
2024-06-01 11:55:18 +0700

    Add Vercel Adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,17 @@
 import { defineConfig } from 'astro/config';
 import db from '@astrojs/db';
-import cloudflare from '@astrojs/cloudflare';
-
 import tailwind from '@astrojs/tailwind';
+
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [db(), tailwind({ applyBaseStyles: false })],
+  integrations: [
+    db(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
   output: 'server',
-  adapter: cloudflare({
-    imageService: 'compile',
-  }),
+  adapter: vercel(),
 });
